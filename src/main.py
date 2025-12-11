d269302f8500f456b4828116c5cacb89f729be2c
ruvinda <ruvindabandaranayake@gmail.com>
2025-12-11 06:57:06 +0530

    v4

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import serial
 import time
+import smbus2
+
 
 class KalmanFilter:
     def __init__(self, Q=0.01, R=1, initial_value=0):
@@ -94,14 +96,168 @@ class PID:
         self.integral = 0
         self.last_time = time.time()
 
+class Camera:
+    cap = None
+    last_frame_time  = None
+    
+    frame_interval = 0.02  # ~20 FPS
+    frame_height = None
+    frame_center_y = None
+    frame_center_x = None
+    frame_width =None
+    @staticmethod
+    def camera_init():
+         
+        
+            url = "http://10.48.134.26:8080/video"
+            Camera.cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+
+            Camera.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 840)
+            Camera.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+            Camera.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"YUYV"))
+            Camera.last_frame_time = time.time()
+
+            if not Camera.cap.isOpened():
+                print("Error: Could not open webcam.")
+                return
+
+            print("Press 'q' to exit the program.")
+    
+    @staticmethod
+    def camera_loop():
+        while True:    
+            ret, frame = Camera.cap.read()
+            if not ret:
+                print("Error: Failed to capture frame.")
+                break
+            current_time = time.time()
+            if current_time - Camera.last_frame_time < Camera.frame_interval:
+                continue
+            Camera.last_frame_time = current_time
+
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            Camera.frame_height,Camera.frame_width ,_ = frame.shape
+            
+    
+            lower_red1 = np.array([0, 120, 10])
+            upper_red1 = np.array([10, 255, 255])
+
+        
+            lower_red2 = np.array([175, 120, 10])
+            upper_red2 = np.array([180, 255, 255])
+
+            mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+            mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+            
+            mask =  mask2+mask1
+
+            kernel = np.ones((5, 5), np.uint8)
+            mask = cv2.erode(mask, kernel, iterations=1)
+            mask = cv2.dilate(mask, kernel, iterations=2)
+
+            contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 
-frame_interval = 0.02  # ~20 FPS
-last_frame_time = 0
+            if contours:
+
+                largest_contour = max(contours, key=cv2.contourArea)
+
+    
+                if cv2.contourArea(largest_contour) > 1000:
+                
+                    x, y, w, h = cv2.boundingRect(largest_contour)
+                    
+                    
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                    
+                    M = cv2.moments(largest_contour)
+                    
+                    if M["m00"] != 0:
+                        Camera.frame_center_y= int(Camera.frame_height/2)
+                        Camera.frame_center_x =  int(Camera.frame_width/2)
+                        
+                        cX = (M["m10"] / M["m00"])
+                        cY = (M["m01"] / M["m00"])
+                        shift_X = Camera.frame_center_x- cX
+                        shift_y = Camera.frame_center_y-cY
+                        if abs(shift_X)<15:
+                            cX= (kfx.update(cX))
+                        if abs(shift_y)<15:
+                            cY=(kfy.update(cY))
+                        cX=int(cX)
+                        cY=int(cY)
+                        
+                        cv2.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
+                        
+                        
+                        text = f"Center: ({cX}, {cY})"
+                        cv2.putText(frame, text, (cX - 20, cY - 20), 
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                        
+                        cv2.line(frame , (cX,cY),( Camera.frame_center_x,Camera.frame_center_y)   , (0,255,0) , 2)
+                        
+                        shoot = False # Initialize before the check
+
+                        # 1. Check if the object is centered
+                        if (y <= Camera.frame_center_y <= y + h) and (x <= Camera.frame_center_x <= x + w):
+                            
+                            # Object is centered! Set shoot flag.
+                            shoot = True
+                            
+                            # 2. Start the timer if it hasn't started yet
+                            if not hasattr(Camera, "shoot_start_time") or Camera.shoot_start_time is None:
+                                Camera.shoot_start_time = time.time()
+                                print("Target centered. 3-second timer started.") # Optional: add feedback
+
+                            # 3. Check if 3 seconds have passed
+                            elif time.time() - Camera.shoot_start_time >= 3:
+                                
+                                # 3 seconds reached! Execute exit actions.
+                                print("3 seconds elapsed. Shooting/Action complete.")
+                                shoot = False
+                                Camera.shoot_start_time = None
+                                move_servo(0, 0, ser, False)
+                                # Assuming this code is inside a loop, this will break it.
+                                break 
+
+                        else:
+                            # Object is NOT centered. Reset the timer.
+                            Camera.shoot_start_time = None
+                            print("Target lost. Timer reset.") # Optional: add feedback
+                        cmd_x , cmd_y = calculate_movemonet(cX,cY ,Camera.frame_center_x,Camera.frame_center_y)
+                        if abs(cmd_x)<0.0025*w:
+                            cmd_x=0
+                
+                            pidx.reset()
+                        if abs(cmd_y)<0.0025*h:
+                            cmd_y=0
+                
+                            pidy.reset()
+                        
+                        move_servo(cmd_x *1.8, cmd_y*1.8 , ser , shoot)
+
+                        
+            cv2.imshow("Red Object Detection", frame)
+
+
+        
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            # Release resources
+        Camera.cap.release()
+        cv2.destroyAllWindows()
+        distance_sensor_read_loop()
+
+
+I2C_ADDR = 0x10   # Arduino I2C address
+bus = smbus2.SMBus(1)  # I2C bus 1 on Raspberry Pi 5
+
+
 prev_p_x = 0
 prev_p_y = 0 
 ser= None
-frame_width ,frame_height  = None , None
 center = 90
 servo_position_x = None
 servo_position_y = None
@@ -112,6 +268,34 @@ pidx = PID(kp=0.8, ki=0, kd=0.01, output_min=0, output_max=180)
 pidy = PID(kp=0.8, ki=0, kd=0.01, output_min=0, output_max=180)
 kfx = KalmanFilter(Q=0.1, R=0.1)
 kfy = KalmanFilter(Q=0.1, R=0.1)
+
+
+
+
+def distance_sensor_read_loop():
+
+    while True:
+        try:
+            data = bus.read_i2c_block_data(I2C_ADDR, 0, 2)
+            angle = data[0]
+            distance = data[1]
+
+            print(f"Object detected → Angle: {angle}°, Distance: {distance} cm")
+
+            time.sleep(0.05)
+            if angle:
+                return distance , angle
+
+        except OSError:
+            # Happens when Arduino didn't send anything yet
+            pass
+
+
+
+def look_at(angle_x , angle_y):
+    #write look at code
+    Camera.camera_loop()
+
 
 def clamp(min_ , max_ , n):
     if n<0:
@@ -172,9 +356,7 @@ def serial_write(ser ,point_x ,point_y  ,shoot):
     if point_x != prev_p_x or  point_y != prev_p_y:
      
      
-        #print(map_the_input(mouse_point[0]))
         i='l'
-        #point_bytes = (str(map_the_input(point_x ,1)) +"x"+str(map_the_input(point_y ,2))+"\n").encode()
         if shoot:
                 i = 's'
         point_bytes = (str(point_x) +"x"+str(point_y)+i+"\n").encode()
@@ -222,139 +404,48 @@ def serial_init():
 
 def map_the_input(input , i):
     if i == 1:
-        output = 180 -int((input/frame_width)*180)
+        output = 180 -int((input/Camera.frame_width)*180)
         return output
     else:
-        output = 180 - int((input/frame_height)*180)
+        output = 180 - int((input/Camera.frame_height)*180)
         return output
 
 
 
 def main():
-    global last_frame_time
-    global frame_height
-    global frame_width
+
 
     ser = serial_init()
     move_to_center(ser)
     
     if ser ==None:
         return
- 
-    url = "http://10.48.134.26:8080/video"
-    cap = cv2.VideoCapture(2, cv2.CAP_V4L2)
+    try: 
+        Camera.camera_init()
+    except Exception as e:
+        print("camera init error"   ,e )
 
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 840)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    
 
-    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"YUYV"))
-
-    if not cap.isOpened():
-        print("Error: Could not open webcam.")
-        return
-
-    print("Press 'q' to exit the program.")
 
     while True:
+        try: 
+            distance , angle  = distance_sensor_read_loop()
+        except Exception as e:
+            print("distance_sensor read error"   ,e )
 
-        ret, frame = cap.read()
-        if not ret:
-            print("Error: Failed to capture frame.")
-            break
-        current_time = time.time()
-        if current_time - last_frame_time < frame_interval:
-            continue
-        last_frame_time = current_time
+        if angle :
+            look_at(distance-100, angle+20)
+            Camera.camera_loop()
 
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        frame_height,frame_width ,_ = frame.shape
+        angle = None
+
+
         
-  
-        lower_red1 = np.array([0, 120, 10])
-        upper_red1 = np.array([10, 255, 255])
+       
 
        
-        lower_red2 = np.array([175, 120, 10])
-        upper_red2 = np.array([180, 255, 255])
 
-        mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
-        mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
-        
-        mask =  mask2+mask1
-
-        kernel = np.ones((5, 5), np.uint8)
-        mask = cv2.erode(mask, kernel, iterations=1)
-        mask = cv2.dilate(mask, kernel, iterations=2)
-
-        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-
-        if contours:
-
-            largest_contour = max(contours, key=cv2.contourArea)
-
-   
-            if cv2.contourArea(largest_contour) > 1000:
-               
-                x, y, w, h = cv2.boundingRect(largest_contour)
-                
-                
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-                
-                M = cv2.moments(largest_contour)
-                
-                if M["m00"] != 0:
-                    frame_center_y= int(frame_height/2)
-                    frame_center_x =  int(frame_width/2)
-                    
-                    cX = (M["m10"] / M["m00"])
-                    cY = (M["m01"] / M["m00"])
-                    shift_X = frame_center_x- cX
-                    shift_y = frame_center_y-cY
-                    if abs(shift_X)<15:
-                        cX= (kfx.update(cX))
-                    if abs(shift_y)<15:
-                        cY=(kfy.update(cY))
-                    cX=int(cX)
-                    cY=int(cY)
-                    
-                    cv2.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
-                    
-                    
-                    text = f"Center: ({cX}, {cY})"
-                    cv2.putText(frame, text, (cX - 20, cY - 20), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                    
-                    cv2.line(frame , (cX,cY),( frame_center_x,frame_center_y)   , (0,255,0) , 2)
-                    
-                    shoot = False
-                    if( y <= (frame_center_y)and frame_center_y <=(y+ h)) and ( x <= (frame_center_x)and frame_center_x <=(x+ w)):
-                        shoot =True
-                    print(abs((y)), (h))
-                    cmd_x , cmd_y = calculate_movemonet(cX,cY ,frame_center_x,frame_center_y)
-                    if abs(cmd_x)<0.0025*w:
-                        cmd_x=0
-             
-                        pidx.reset()
-                    if abs(cmd_y)<0.0025*h:
-                        cmd_y=0
-              
-                        pidy.reset()
-                    
-                    move_servo(cmd_x *1.8, cmd_y*1.8 , ser , shoot)
-
-                    
-        cv2.imshow("Red Object Detection", frame)
-        # cv2.imshow("Mask", mask) # Uncomment to see what the computer 'sees'
-
-       
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Release resources
-    cap.release()
-    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
